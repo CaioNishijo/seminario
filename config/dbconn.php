c8635de28dd28dd8a685be853a5840f87ec39354
Caio Nishijo <cknishijo1@gmail.com>
2024-11-29 14:01:09 -0300

    criando variáveis de ambiente

--- config/dbconn.php
+++ config/dbconn.php
@@ -1,9 +1,13 @@
 <?php
-    $host = 'localhost';
-    $db = 'projeto_consultas';
-    $usuario = 'root';
-    $port = 3306;
-    $senha = '';
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->load();
+
+    $host = $_ENV["DB_HOST"];
+    $db = $_ENV["DB_NAME"];
+    $usuario = $_ENV["DB_USER"];
+    $port = $_ENV["DB_PORT"];
+    $senha = $_ENV["DB_PASSWD"];
 
     try{
         $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $usuario, $senha);
